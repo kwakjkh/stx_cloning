8c6bc14a74b0330208756f45becc7fc4ac05e631
kwakjkh <1210kwakjkh@gmail.com>
2024-07-01 12:56:28 +0900

    버튼 추가

--- js/main.js
+++ js/main.js
@@ -15,4 +15,33 @@ function scrollToSection(sectionId) {
         section.scrollIntoView({ behavior : "smooth" });
     }
 }
+// top버튼 스크롤 기능------------
+ const topBtn = document.getElementById("top-btn")
+ topBtn.addEventListener("click" , function(event){
+    event.preventDefault();
+    console.log(window.scrollY);
+    if (window.scrollY == 0){
+        window.scrollTo({
+            top:3197,
+            behavior: "smooth"
+        });
+    }
+    else{
+        window.scrollTo({
+            top:0,
+            behavior: "smooth"
+        });
+    }
+ });
+    // 화살표 이미지 회전
+    const topBtnImg = document.getElementById("top-btn-img")
+    window.addEventListener("scroll" , function (scTop) {
+        scTop = window.document.documentElement.scrollTop;
+        if(scTop > 0){
+            topBtnImg.classList.add("up")
+        }
+        else{
+            topBtnImg.classList.remove("up")
+        }
+    });
 };
